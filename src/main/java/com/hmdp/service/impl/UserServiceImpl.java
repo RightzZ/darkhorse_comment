b42579214abe50b21ba8bc995ae9f780fd6fafbb
rightzz <xkyyes@gmail.com>
2025-07-12 23:18:55 +0900

    Functions about login

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,30 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -14,7 +34,88 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 如果不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+        
+        // 如果符合，生成一个验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 将验证码存入redis
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
+
+        // 发送验证码
+        log.debug("发送短信验证码成功，验证码：{}", code);
+
+        // 返回ok
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 校验手机号和验证码
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 如果不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+
+        // 从redis获取验证码并校验
+        Object cacheChde = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
+        String code = loginForm.getCode();
+        if (cacheChde == null || !cacheChde.toString().equals(code)) {
+            // 不一致，报错
+            return Result.fail("验证码错误！");
+        }
+
+        // 如果一致，根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+
+        // 判断用户是否存在
+        if (user == null) {
+            // 不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+
+        // 保存用户信息到redis中
+        // 随机生成一个token
+        String token = UUID.randomUUID().toString(true);
+
+        // 将user对象转换为HashMap存储
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+
+        // 存储到redis
+        String s = LOGIN_USER_KEY + token;
+        stringRedisTemplate.opsForHash().putAll(s, userMap);
+
+        // 设置有效期
+        stringRedisTemplate.expire(s, LOGIN_USER_TTL, TimeUnit.MINUTES);
+
+        // 返回token
+        return Result.ok(token);
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+
+        save(user);
+        return user;
+    }
 }
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,27 @@
+package com.hmdp.utils;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 判断是否需要去拦截
+        if(UserHolder.getUser() == null) {
+            response.setStatus(401);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
